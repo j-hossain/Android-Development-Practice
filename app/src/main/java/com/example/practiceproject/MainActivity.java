a65dbab20d72ac1ac020246e91099c85bf0451b2
d-joker <jahin.hsn101@gmail.com>
2023-03-23 13:45:46 +0600

    Guess number done

--- app/src/main/java/com/example/practiceproject/MainActivity.java
+++ app/src/main/java/com/example/practiceproject/MainActivity.java
@@ -7,10 +7,22 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    int randomNumber;
+    public void NumberGuessed(View view){
+        EditText editTextGuessNumber = findViewById(R.id.editTextGuessNumber);
+        int guessedNumber = Integer.parseInt(editTextGuessNumber.getText().toString());
+        if(randomNumber<guessedNumber) Toast.makeText(this, "Lower", Toast.LENGTH_SHORT).show();
+        else if(randomNumber>guessedNumber) Toast.makeText(this, "Upper", Toast.LENGTH_SHORT).show();
+        else if(randomNumber==guessedNumber) Toast.makeText(this, "Guessed", Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Random ran = new Random();
+        randomNumber = ran.nextInt(19)+1;
     }
 }
