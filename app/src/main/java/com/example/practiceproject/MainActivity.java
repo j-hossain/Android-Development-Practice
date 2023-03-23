6ecb1834d2ba5de24a87e1c1d232b72d752c277b
d-joker <jahin.hsn101@gmail.com>
2023-03-23 12:48:58 +0600

    Togles image by pressing button

--- app/src/main/java/com/example/practiceproject/MainActivity.java
+++ app/src/main/java/com/example/practiceproject/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.practiceproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.imageView).setTag("1");
+    }
+    public void imageCHangerButtonClicked(View view){
+        ImageView imageView = findViewById(R.id.imageView);
+        if(imageView.getTag()=="1"){
+            imageView.setImageResource(R.drawable.image_2);
+            imageView.setTag("2");
+        }
+        else{
+            imageView.setImageResource(R.drawable.image_1);
+            imageView.setTag("1");
+        }
     }
 }
