a897738f1033788b083ab27fe7884c4e9fd7cd1d
d-joker <jahin.hsn101@gmail.com>
2023-03-23 13:37:49 +0600

    COnverter code removed

--- app/src/main/java/com/example/practiceproject/MainActivity.java
+++ app/src/main/java/com/example/practiceproject/MainActivity.java
@@ -8,12 +8,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public void ConverterButtonClicked(View view){
-        EditText amountInTaka = findViewById(R.id.editTextAmountTaka);
-        double takaAmount = Double.parseDouble(amountInTaka.getText().toString());
-        double dollarAmount = takaAmount * 0.0095;
-        Toast.makeText(this, String.format("%.2f",dollarAmount), Toast.LENGTH_SHORT).show();
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
