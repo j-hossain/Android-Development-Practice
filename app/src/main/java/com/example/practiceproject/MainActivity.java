851fc4634430dd2caac58c7f7c4df00eacfd032c
d-joker <jahin.hsn101@gmail.com>
2023-03-23 12:17:26 +0600

    Login Page that makes a toast

--- app/src/main/java/com/example/practiceproject/MainActivity.java
+++ app/src/main/java/com/example/practiceproject/MainActivity.java
@@ -3,9 +3,17 @@ package com.example.practiceproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void LoginButtonClicked(View view){
+        EditText editTextUsername = findViewById(R.id.editTextUsername);
+        EditText editTestPassword = findViewById(R.id.editTextPassword);
+        Toast.makeText(this, "You Entered\nUsername: "+editTextUsername.getText().toString()+"\nPassword: "+editTestPassword.getText().toString()+"\n", Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
